de55910b3c10c40326422e71fd28dd20f97cc772
TaeJirapan <Jirapanza_14@hotmail.com>
2017-09-15 09:36:37 +0700

    W1

---
+++ Work01/Work01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHello();
+            Console.ReadLine();
+        }
+
+        void sayHello()
+        {
+            Console.WriteLine("Hello World.");
+
+        }
+    }
+}
